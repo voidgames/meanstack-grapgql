e77652cd64ae6e2aa08e003bc569a0a15a9962d4
void.operator <void.operator.mail@gmail.com>
2019-02-23 01:02:18 +0900

    add express server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# meanstack-grapgql
-MEAN full stack App with GraphQL (Apollo)
+# MEAN-Stack-Grapgql
+* Grapchqlの勉強用リポジトリ
+* MEANスタックで構築
+* アプリ概要はREADME.md, その他構築時のメモなどはdocument.mdに記載する
---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello, World!'));
+
+app.listen(4000, () => console.log('Express server running on port 4000'));
